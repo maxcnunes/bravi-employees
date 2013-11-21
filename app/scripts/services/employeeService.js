e2757fb6cd2d41aaf15572d36280ba84958cae53
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-21 20:45:05 -0200

    creating CRUD operations in the service

--- app/scripts/services/employeeService.js
+++ app/scripts/services/employeeService.js
@@ -2,11 +2,28 @@
 
 angular.module('braviEmployeesApp')
   .service('EmployeeService', function EmployeeService($http) {
-    var getAll = function(){
-      return $http.get('/api/employees');
+    var httpPath = '/api/employees',
+    getAll = function(){
+      return $http.get(httpPath);
+    },
+    getById = function(id){
+      return $http.get(httpPath + '/' + id);
+    },
+    create = function(model){
+      return $http.post(httpPath, model);
+    },
+    update = function(model){
+      return $http.put(httpPath + '/' + model.id, model);
+    },
+    remove = function(id){
+      return $http.delete(httpPath + '/' + id);
     };
 
     return {
-      getAll : getAll
+      getAll: getAll,
+      getById: getById,
+      create: create,
+      update: update,
+      remove: remove
     };
   });
--- test/spec/services/employeeService.js
+++ test/spec/services/employeeService.js
@@ -6,20 +6,66 @@ describe('Service: EmployeeService', function () {
   beforeEach(module('braviEmployeesApp'));
 
   var EmployeeService,
-      $httpBackend;
+      $httpBackend,
+      fakeEmployee;
 
   beforeEach(inject(function (_$httpBackend_, _EmployeeService_) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/employees')
-      .respond(['Employee1', 'Employee2', 'Employee3']);
     EmployeeService = _EmployeeService_;
+    fakeEmployee = { id: 1 };
   }));
 
+  afterEach(function() {
+    $httpBackend.flush();
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should get list employees', function () {
+    $httpBackend.expectGET('/api/employees').respond(['Employee1', 'Employee2', 'Employee3']);
+
     var result = EmployeeService.getAll();
     expect(result).toBeDefined();
     result.success(function(employees){ expect(employees.length).toBeGreaterThan(0); });
-    $httpBackend.flush();
+  });
+
+
+  it('should get employee by id', function () {
+    $httpBackend.expectGET('/api/employees/'+fakeEmployee.id).respond(fakeEmployee);
+
+    var result = EmployeeService.getById(fakeEmployee.id);
+    expect(result).toBeDefined();
+    result.success(function(employee){ expect(employee).toBe(fakeEmployee); });
+  });
+
+
+  it('should create employee', function () {
+    $httpBackend.expectPOST('/api/employees', fakeEmployee).respond(201, '');
+
+    var result = EmployeeService.create(fakeEmployee);
+
+    expect(result).toBeDefined();
+    result.success(function(res,sts){ expect(sts).toBe(201); });
+  });
+
+
+  it('should update employee', function () {
+    $httpBackend.expectPUT('/api/employees/'+fakeEmployee.id, fakeEmployee).respond(200, '');
+
+    var result = EmployeeService.update(fakeEmployee);
+
+    expect(result).toBeDefined();
+    result.success(function(res,sts){ expect(sts).toBe(200); });
+  });
+
+
+  it('should remove employee', function () {
+    $httpBackend.expectDELETE('/api/employees/'+fakeEmployee.id).respond(200, '');
+
+    var result = EmployeeService.remove(fakeEmployee.id);
+
+    expect(result).toBeDefined();
+    result.success(function(res,sts){ expect(sts).toBe(200); });
   });
 
 });

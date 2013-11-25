0a1005b8425f64ce408d3ffd61f2e51ca9eefd98
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-25 02:05:31 -0200

    :sleeping: added filter by known technologies

--- app/scripts/controllers/employee-list.js
+++ app/scripts/controllers/employee-list.js
@@ -2,15 +2,29 @@
 
 angular.module('braviEmployeesApp')
   .controller('EmployeeListCtrl', function ($scope, EmployeeService) {
-    $scope.employees = [];
+    /* Config Grid */
     $scope.gridOptions = {
       data: 'employees',
+      enableSorting: true,
+      filterOptions: {
+        filterText: '',
+        inputFilterText: ''
+      },
       columnDefs: [
         { field: 'name', displayName: 'Name', width: 150 },
         { field: 'birthDate', displayName: 'Birth', cellFilter: 'date: "dd/MM/yyyy"', width: 90 },
         { field: 'githubUsername', displayName: 'Github', width: 100 },
-        { field: 'knownTechnologies', displayName: 'Known Technologies', cellFilter: 'join', sortable: false },
+        { field: 'knownTechnologies', displayName: 'Known Technologies', cellFilter: 'join',
+          sortable: false, colFilterText: ''
+        },
       ]};
+
+    /* Filter by known technologies */
+    $scope.$watch('gridOptions.filterOptions.inputFilterText', function(searchText, oldsearchText) {
+      if (searchText !== oldsearchText) {
+        $scope.gridOptions.filterOptions.filterText = "knownTechnologies:" + searchText + "; ";
+      }
+    });
 
     EmployeeService.getAll().success(function(employees) {
       $scope.employees = employees;
